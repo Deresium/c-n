4dbf9d9c309cd9b2c200a9421e42dcc07a3c3956
Dimitri Steinbusch <dimitri.steinbusch@creatiview.be>
2021-05-26 19:19:54 +0200

    http to https

--- dist/middlewares/redirectHttps.js
+++ dist/middlewares/redirectHttps.js
@@ -1,11 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const redirectHttps = (req, res, next) => {
-    var _a;
-    if ((_a = req.headers.host) === null || _a === void 0 ? void 0 : _a.includes('captiveportal.')) {
-        next();
-    }
-    else if (req.header('x-forwarded-proto') !== 'https')
+    if (req.header('x-forwarded-proto') !== 'https')
         res.redirect(`https://${req.hostname}${req.url}`);
     else if (!req.hostname.startsWith('www.'))
         res.redirect(`https://www.${req.hostname}${req.url}`);
