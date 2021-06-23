b26ad2b5e631217171ef11c3b25dfca265a75759
Dimitri Steinbusch <dimitri.steinbusch@creatiview.be>
2021-06-23 16:58:28 +0200

    change portail captif

--- dist/middlewares/returnIndex.js
+++ dist/middlewares/returnIndex.js
@@ -5,12 +5,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const path_1 = __importDefault(require("path"));
 const returnIndex = (req, res, next) => {
-    var _a, _b, _c;
+    var _a, _b, _c, _d, _e;
     if (((_a = req.headers.accept) === null || _a === void 0 ? void 0 : _a.includes('text/html')) && ((_b = req.headers.host) === null || _b === void 0 ? void 0 : _b.includes('captiveportal'))) {
         const captivePortalFile = path_1.default.join(__dirname, '../../public/captiveportal.html');
         res.sendFile(captivePortalFile);
     }
-    else if (((_c = req.headers.accept) === null || _c === void 0 ? void 0 : _c.includes('text/html')) && !req.url.includes('solutionsfiles')) {
+    else if (((_c = req.headers.accept) === null || _c === void 0 ? void 0 : _c.includes('text/html')) && ((_d = req.headers.host) === null || _d === void 0 ? void 0 : _d.includes('portailcaptif'))) {
+        const portailCaptifFile = path_1.default.join(__dirname, '../../public/portailcaptif.html');
+        res.sendFile(portailCaptifFile);
+    }
+    else if (((_e = req.headers.accept) === null || _e === void 0 ? void 0 : _e.includes('text/html')) && !req.url.includes('solutionsfiles')) {
         const publicDirectoryPath = path_1.default.join(__dirname, '../../public/c-n');
         res.sendFile(publicDirectoryPath + '/index.html');
     }
--- src/middlewares/returnIndex.ts
+++ src/middlewares/returnIndex.ts
@@ -6,6 +6,10 @@ const returnIndex: RequestHandler = (req, res, next) => {
         const captivePortalFile = path.join(__dirname, '../../public/captiveportal.html');
         res.sendFile(captivePortalFile);
     }
+    else if(req.headers.accept?.includes('text/html') && req.headers.host?.includes('portailcaptif')){
+        const portailCaptifFile = path.join(__dirname, '../../public/portailcaptif.html');
+        res.sendFile(portailCaptifFile);
+    }
     else if(req.headers.accept?.includes('text/html') && !req.url.includes('solutionsfiles')){
         const publicDirectoryPath = path.join(__dirname, '../../public/c-n');
         res.sendFile(publicDirectoryPath + '/index.html');
